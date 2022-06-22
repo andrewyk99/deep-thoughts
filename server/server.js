330aca87aa9b8f8d3162ef2cc608f93d6e9ed5cb
andrewyk99 <andrewyk99@hotmail.com>
2022-06-22 00:33:47 -0600

    set up full-stack structure

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // Import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -27,6 +28,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // Integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
